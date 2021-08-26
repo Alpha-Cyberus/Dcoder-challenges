05b1d8543eb95152a254b8001f4828f0db6bbca4
Luffy O'Brien <63784876+Alpha-Cyberus@users.noreply.github.com>
2021-08-26 10:31:00 +0200

    Update Rotate_Array.py

--- Python/Easy/Rotate_Array.py
+++ Python/Easy/Rotate_Array.py
@@ -1,16 +1,15 @@
 iN,iK = input().split()
 iA = input().split()
-
+for i in range(len(iA)):
+  iA[i] = int(iA[i])
   
 def rotateArray(n, k, arr):
   arrTemp = []
   for i in range(k):
-    arrTemp.append(arr[0])
-    arr.pop(0)
-    arr.append(arrTemp[i])
-  arrTemp.clear  
+    arrTemp.append(arr[n-1])
+    arr.pop(n-1)
+    arr.insert(0, arrTemp[i])
   print(*arr, sep=" ")
   return arr
-
 
 rotateArray(int(iN), int(iK), iA)
